bb235df0430a8c2d728a2a7cb8e963d6cbb69be0
Ahn, seung-rye <seungrye@gmail.com>
2025-07-21 23:22:00 +0900

    fix: 게시글 업데이트 후 존재하지 않는 경우에 대한 오류 처리 추가

--- src/app/api/like-dislike/route.tsx
+++ src/app/api/like-dislike/route.tsx
@@ -36,6 +36,10 @@ export async function POST(req: Request) {
       { new: true } // ✅ 업데이트된 값을 반환
     );
 
+    if (!updatedPost) {
+      return NextResponse.json({ message: "게시글을 찾을 수 없습니다." }, { status: HttpStatusCode.NotFound });
+    }
+
     await checkAndGrantPostInteractionAchievements(payload._id);
 
     return NextResponse.json({ message: "Like/Dislike 업데이트 성공", likes: updatedPost.likes }, { status: HttpStatusCode.Ok });
